c38c312b45a3cbc3330a68e2b963bf6568087df8
Beqaelbaqidze <83675524+Beqaelbaqidze@users.noreply.github.com>
2023-08-08 03:53:09 -0700

    commit

--- main.class.js
+++ main.class.js
@@ -1,8 +1,10 @@
-// main.class.js
 export class mainClass {
-  htmlLITpl = (value, childrenHTML) =>
-    `<div class="node">${value}${childrenHTML}</div>`;
-  htmlUlTpl = (li) => `${li}`;
+  htmlLITpl = (id, value, hasChildren) =>
+    `<div class="node" data-id="${id}">
+      <button class="nodebtn ${hasChildren ? "exChild" : ""}">+</button>
+      ${value}
+      ${hasChildren ? '<ul class="children hidden"></ul>' : ""}
+    </div>`;
 
   constructor() {}
 
@@ -10,28 +12,68 @@ export class mainClass {
     const assembledHTML = this.buildHTML(data);
     document.querySelector(selector).innerHTML = assembledHTML;
 
-    const parentNodes = document.querySelectorAll(".node");
-    parentNodes.forEach((node) => {
-      node.addEventListener("click", this.toggleChildren);
-    });
+    const container = document.querySelector(selector);
+    container.addEventListener("click", this.handleButtonClick.bind(this));
   }
 
   buildHTML(data) {
     let vHTML = "";
     for (const item of data) {
-      const children = item.children
-        ? this.htmlUlTpl(this.buildHTML(item.children))
-        : "";
-      vHTML += this.htmlLITpl(item.text, children);
+      const hasChildren = item.children && item.children.length > 0;
+      vHTML += this.htmlLITpl(item.id, item.text, hasChildren);
     }
     return vHTML;
   }
 
-  toggleChildren(event) {
-    const target = event.currentTarget;
-    const children = target.querySelector(".children");
-    if (children) {
-      children.add.mainClass("hidden");
+  handleButtonClick(event) {
+    const target = event.target;
+    if (
+      target.classList.contains("nodebtn") &&
+      target.classList.contains("exChild")
+    ) {
+      event.preventDefault();
+      const parent = target.closest(".node");
+      const children = parent.querySelector(".children");
+      const parentId = parent.dataset.id;
+
+      if (children.classList.contains("loaded")) {
+        const requestConfig = {
+          url: "/data.json",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        fetch(requestConfig.url, {
+          method: requestConfig.method,
+          headers: requestConfig.headers,
+        })
+          .then((response) => response.json())
+          .then((value) => {
+            const childrenData = value.filter(
+              (item) => item.parentId === parentId
+            );
+            if (childrenData.length > 0) {
+              children.innerHTML = this.buildHTML(childrenData);
+              children.classList.remove("hidden");
+              children.classList.add("loaded");
+              
+              const childNodes = children.querySelectorAll(".nodebtn.exChild");
+              childNodes.forEach((nodebtn) => {
+                nodebtn.addEventListener(
+                  "click",
+                  this.handleButtonClick.bind(this)
+                );
+              });
+            }
+          })
+          .catch((error) => {
+            console.error("Error", error);
+          });
+      } else {
+        children.classList.toggle("hidden");
+      }
     }
   }
 }
